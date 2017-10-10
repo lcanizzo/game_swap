febdbf1d3eb3e87bb68da1f7eb41a17bafe48f50
Francisco Carrera <fc1159@nyu.edu>
2017-10-10 10:50:25 -0400

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,26 @@
 // Set up MySQL connection.
+var mysql = require("mysql");
 
-
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "gameswap_db"
+});
 
 // Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
-
+connection.on('error', function(err) {
+    console.log(err.code); // 'ER_BAD_DB_ERROR'
+});
 
 // Export connection for our ORM to use.
+module.exports = connection;
