e3373e6bb2c19edddf0162b3b767ff4d3d17b7b0
Francisco Carrera <fc1159@nyu.edu>
2017-10-10 11:18:59 -0400

    Now displaying landing page on server startup. files renamed to not seem like placeholders

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,11 +4,11 @@ var express = require("express")
 var router = express.Router();
 
 // Import the model to use its database functions.
-var game = require("../models/exampleModel.js");
+var game = require("../models/model.js");
 // Create all our routes and set up logic within those routes where required.
 //Login Page
 router.get("/", function(request, response){
-    response.render("index")
+    response.render("index", {layout: 'landing'})
     console.log("Working Login")
 })
 
