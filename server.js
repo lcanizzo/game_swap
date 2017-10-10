80f9904717334f8790409d37e045e5dbf55901c3
Andrew Wybo <andrewwybo@gmail.com>
2017-10-10 09:43:22 -0400

    added require express, bodyparser and express handlebars, set up working port, set up handlebars

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 // Import express package
+var express = require('express')
 // Import bodyParse package
+var bodyParser = require('body-parser')
+var exphbrs = require('express-handlebars')
+
+var app = express();
 
 // Establish port
-
+var PORT = process.env.PORT || 8000;
 // Serve static content for the app from the "public" directory in the application directory.
 
 // Set Handlebars.
-
-
+app.engine("handlebars", exphbrs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // Import routes from Controllers and give the server access to them.
+
+
+app.listen(PORT, function(){
+    console.log("Listening on Port: " + PORT)
+})
