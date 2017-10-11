da15a01d3b412e7e7a92dbbe6a093b2caa826b05
Wyb0 <andrewwybo@gmail.com>
2017-10-10 21:37:57 -0400

    added working for loop and populating game results/working

--- config/apiSearch.js
+++ config/apiSearch.js
@@ -19,19 +19,26 @@ var gameResults = [];
 //call for game
 client.games({
     fields: '*', // Return all fields
-    limit: 1, // Limit results
+    limit: 5, // Limit results
     offset: 0, // Index offset for results
     search: string
 }).then(function(response){
-    console.log(response.body[0].name)
-    let imageId = response.body[0].cover.cloudinary_id
-    console.log("image ID", response.body[0].cover.cloudinary_id)
+    for (i=0; i < response.body.length; i++) {
+    //console.log(response.body[i].name)
+    let imageId = response.body[i].cover.cloudinary_id
+    //console.log("image ID", response.body[i].cover.cloudinary_id)
     // response.body contains the parsed JSON response to this query
     let image = client.image({
         cloudinary_id: imageId, 
     }, 'cover_big', 'jpg')
-    console.log("Image link", image)
+    //console.log("Image link", image)
+    var newGame = new game(response.body[i].name, image)
+    //console.log("New Game: ", newGame)
+    gameResults.push(newGame)
+}
+console.log("New Game: ", gameResults)
 }).catch(function(error){
     throw error;
 });
+
 
