03a2f6bc7a5e29f15ca1413c656f044db76000cd
Andrew Wybo <andrewwybo@gmail.com>
2017-10-11 10:36:11 -0400

    set if statement for cases where cover art is not available, set img link for cover art not available

--- config/apiSearch.js
+++ config/apiSearch.js
@@ -6,7 +6,8 @@ var client = igdb('be1ea7dccb14bcf3ae57b1e16d62cb74');
 
 //variable for game string
 //********************************* HARD CODED TITLE, NEEDS TO BE REPLACED WITH USER INPUT********************************** */
-let string = "halo";
+//********************************** POSSIBLE TO CONCATONATE PLATFORM TO END OF TITLE, EX DOOM XBOX ONE BRINGS UP DIF THEN DOOM */
+let string = "Doom xbox one";
 
 //constructor function for creating game
 var game = function(name, image) {
@@ -19,22 +20,31 @@ var gameResults = [];
 
 //function for game search
 var gameSearch = function(search){
+    gameResults = [];
     //call for game
     client.games({
     fields: '*', // Return all fields
-    limit: 5, // Limit results
+    limit: 1, // Limit results
     offset: 0, // Index offset for results
-    search: search
+    //order: 'release_dates.date:desc',
+    search: string
 }).then(function(response){
     gameResults=[];
     for (i=0; i < response.body.length; i++) {
+    let image;
     //console.log(response.body[i].name)
+    //console.log(response.body[i])
+    if (response.body[i].cover){
     let imageId = response.body[i].cover.cloudinary_id
     //console.log("image ID", response.body[i].cover.cloudinary_id)
     // response.body contains the parsed JSON response to this query
-    let image = client.image({
+    image = client.image({
         cloudinary_id: imageId, 
     }, 'cover_big', 'jpg')
+}
+    else {
+        image = "//publications.iarc.fr/uploads/media/default/0001/02/thumb_1199_default_publication.jpeg"
+    }
     //console.log("Image link", image)
     var newGame = new game(response.body[i].name, image)
     //console.log("New Game: ", newGame)
@@ -45,5 +55,8 @@ console.log("New Game: ", gameResults)
     throw error;
 });
 }
+
+gameSearch(string)
 //export
 module.exports = gameSearch
+
