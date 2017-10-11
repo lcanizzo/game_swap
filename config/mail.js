c56e57c17c469479f4156903449e4d196928b311
Andrew Wybo <andrewwybo@gmail.com>
2017-10-11 12:38:33 -0400

    added var for basic email message for trade request

--- config/mail.js
+++ config/mail.js
@@ -16,3 +16,15 @@ nodemailer.createTransport({
     }
 });
 
+//variable for email message to send to user
+let mailOptions = {
+    //our email address
+    from: '"Game Swap" <gameswapu@gmail.com>',
+    //user email
+    to: '', //****NEEDS TO BE SET TO VAR FOR USER EMAIL*****/
+    //subject line
+    subject: 'You currently have a trade request!',
+    //text and html
+    text: 'You currently have a trade request!', 
+    html: '<b>You currently have a trade request!</b>' 
+};
